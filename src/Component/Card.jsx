acc30cb316b9d8cf15743fcaba68acbb12dc431c
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-19 13:42:10 +0530

    card logic

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-
-const Card = ({ card }) => {
+export const Card = ({ card, onClick }) => {
   return (
-    <div className='card'>
-        <div className='card-front'>?</div>
-        <div className='card-back'>{card}</div>
+    <div
+      className={`card ${card.isFlipped ? "flipped" : ""} ${
+        card.isMatched ? "matched" : ""
+      }`}
+      onClick={() => onClick(card)}
+    >
+      <div className="card-front">?</div>
+      <div className="card-back">{card.value}</div>
     </div>
-  )
-}
-
-export default Card
+  );
+};
