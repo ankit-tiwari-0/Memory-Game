d5678276d76b2d459e195dc4db152f3d36fa9d0b
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-19 12:58:38 +0530

    Add cardval

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import Header from './Component/Header'
+import Card from './Component/Card'
+
+const cardval = [
+  "🍌",
+  "🍇",
+  "🍉",
+  "🍒",
+  "🍓",
+  "🍍",
+  "🍑",
+  "🥑",
+  "🍒",
+  "🍇",
+  "🍍",
+  "🍉",
+  "🍓",
+  "🍌",
+  "🥑",
+  "🍑",
+]
 
 const App = () => {
   return (
     <div className='app'>
       <Header score={3} moves={0}  />
+
+      <div className='cards-grid'>
+        {
+         cardval.map((card) => (
+           <Card card = {card} />  
+         ))
+        }
+      </div>
     </div>
   )
 }
